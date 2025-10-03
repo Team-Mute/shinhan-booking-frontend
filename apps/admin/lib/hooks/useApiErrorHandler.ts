c1bf467cce8cf4f56eaa158432bd665ad5dcd1a9
eunbicho <puppy101976@naver.com>
2025-10-03 16:57:58 +0900

    Feat: api 에러 발생 시 모달 띄우는 훅 추가

---
+++ apps/admin/lib/hooks/useApiErrorHandler.ts
@@ -0,0 +1,11 @@
+import { useModalStore } from "@admin/store/modalStore";
+
+export function useApiErrorHandler() {
+  const { open } = useModalStore();
+
+  return (error: any) => {
+    // axios에서 붙인 customMessage 사용
+    const message = error.customMessage || "알 수 없는 오류가 발생했습니다.";
+    open("안내", message);
+  };
+}
