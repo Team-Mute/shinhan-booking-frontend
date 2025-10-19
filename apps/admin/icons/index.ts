4a6a54f12cb53914a0151f9b8bd9b8fd130c5138
eunbicho <puppy101976@naver.com>
2025-10-19 21:55:49 +0900

    Fix: svg 파일들 경로 변경

--- apps/admin/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DashboardIcon } from "./dashboard.svg";
-export { default as ReservationIcon } from "./reservation.svg";
-export { default as SpaceIcon } from "./space.svg";
-export { default as AddUserIcon } from "./adduser.svg";
-export { default as LogoutIcon } from "./logout.svg";
