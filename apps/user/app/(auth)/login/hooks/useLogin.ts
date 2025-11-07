1153c9f6a8f8a49966a7c0b10670620de2b064bb
eunbicho <puppy101976@naver.com>
2025-11-07 11:17:06 +0900

    Feat: 사용자 로그인 페이지 추가

---
+++ apps/user/app/(auth)/login/hooks/useLogin.ts
@@ -0,0 +1,68 @@
+import { useApiErrorHandler } from "@admin/lib/hooks/useApiErrorHandler";
+import { useModalStore } from "@admin/store/modalStore";
+import { loginApi } from "@user/lib/api/userAuth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+/**
+ * useLogin 훅
+ * ----------------------------
+ * 사용자 로그인 화면에서 사용할 상태와 로직을 관리하는 커스텀 훅
+ *
+ * @description
+ * - 이메일과 비밀번호 입력 상태를 관리.
+ * - 로그인 버튼 클릭 시 `loginApi`를 호출하여 인증을 시도.
+ * - 로그인 성공 시 메인 페이지(`/`)로 이동.
+ * - 실패 시 상태 코드에 따라 모달을 띄우거나 공통 에러 핸들러를 실행.
+ *
+ */
+export function useLogin() {
+  const { open } = useModalStore();
+  const router = useRouter();
+  const handleError = useApiErrorHandler();
+
+  // --- 입력 상태 관리 ---
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /**
+   * @description 이메일 입력 시 상태 업데이트
+   */
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  /**
+   * @description 비밀번호 입력 시 상태 업데이트
+   */
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  /**
+   * @description 로그인 요청 처리
+   * - 이메일/비밀번호를 `adminLoginApi`로 전송
+   * - 성공 시 홈(`/`)으로 이동
+   * - 실패 시 상태 코드에 따라 다른 모달 메시지 표시
+   */
+  const handleLogin = async () => {
+    try {
+      await loginApi(email, password);
+      router.push("/");
+    } catch (err: any) {
+      if (err?.status === 401) {
+        open("안내", "로그인에 실패했습니다.");
+      } else {
+        handleError(err);
+      }
+    }
+  };
+
+  return {
+    email,
+    password,
+    handleEmailChange,
+    handlePasswordChange,
+    handleLogin,
+  };
+}
---
+++ apps/user/app/(auth)/login/page.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import React from "react";
+import styled from "@emotion/styled";
+import Input from "@components/ui/input/Input";
+import Button from "@components/ui/button/Button";
+import Loader from "@user/components/Loader";
+import { useRouter } from "next/navigation";
+import { isValidEmail } from "@user/lib/validators/email";
+import colors from "@styles/theme";
+import { useLogin } from "./hooks/useLogin";
+
+/**
+ * LoginPage 컴포넌트
+ * ----------------------------
+ * 사용자 로그인 페이지
+ * - 이메일과 비밀번호 입력 폼 제공
+ * - 상태 및 비즈니스 로직은 useLogin 훅에서 관리
+ */
+export default function LoginPage() {
+  const {
+    password,
+    email,
+    handleEmailChange,
+    handlePasswordChange,
+    handleLogin,
+  } = useLogin();
+  const router = useRouter();
+
+  return (
+    <Container>
+      <Loader>
+        <GreetingText>
+          안녕하세요,
+          <br />
+          신한금융희망재단입니다.
+        </GreetingText>
+
+        <LoginForm
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
+          {/* 이메일 입력 */}
+          <InputWrapper>
+            <Input
+              type="email"
+              placeholder="이메일"
+              value={email}
+              onChange={handleEmailChange}
+              errorMessage={
+                email && !isValidEmail(email)
+                  ? "이메일 형식이 맞지 않습니다."
+                  : ""
+              }
+            />
+          </InputWrapper>
+
+          {/* 비밀번호 입력 */}
+          <InputWrapper>
+            <Input
+              type="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </InputWrapper>
+
+          {/* 비밀번호 찾기 버튼 */}
+          <FindPasswordButton
+            type="button"
+            onClick={() => {
+              router.push("/reset-password");
+            }}
+          >
+            비밀번호 찾기
+          </FindPasswordButton>
+
+          {/* 로그인 버튼 */}
+          <Button
+            type="submit"
+            isActive={isValidEmail(email) && !!password}
+            disabled={!(isValidEmail(email) && !!password)}
+          >
+            로그인
+          </Button>
+        </LoginForm>
+        <BottomText>
+          아직 회원이 아니신가요?
+          <SignUpButton type="button" href="/signup">
+            회원가입
+          </SignUpButton>
+        </BottomText>
+      </Loader>
+    </Container>
+  );
+}
+
+// --- styled ---
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+`;
+
+const GreetingText = styled.h2`
+  font-size: 24px;
+  line-height: 1.5;
+  text-align: center;
+  margin-bottom: 40px;
+  font-weight: 500;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+
+  & > input:nth-of-type(2) {
+    margin-top: 12px; /* 이메일 → 비밀번호 */
+  }
+
+  & > button[type="button"]:first-of-type {
+    margin-top: 12px; /* 비밀번호 → 비밀번호 찾기 */
+  }
+
+  & > button[type="submit"] {
+    margin-top: 32px; /* 비밀번호 찾기 → 로그인 */
+  }
+`;
+
+const FindPasswordButton = styled.button`
+  background: none;
+  border: none;
+  color: #555;
+  font-size: 14px;
+  cursor: pointer;
+  align-self: flex-end;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 12px;
+  width: 22rem;
+`;
+
+const BottomText = styled.div`
+  margin-top: 12px; /* 로그인 → 하단 문구 */
+  font-size: 14px;
+  color: ${colors.graycolor50};
+`;
+
+const SignUpButton = styled.a`
+  background: none;
+  border: none;
+  color: ${colors.graycolor100}
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 4px;
+  text-decoration: underline;
+  text-decoration-thickness: 1px;
+  text-decoration-color: ${colors.graycolor100};
+`;
