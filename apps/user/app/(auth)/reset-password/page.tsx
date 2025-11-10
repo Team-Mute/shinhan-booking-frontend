6feac656b100cf098e1ab1af6f564f8aa6e1d60d
eunbicho <puppy101976@naver.com>
2025-11-10 10:49:01 +0900

    Style: 사용자 비밀번호 재설정 페이지 top 여백 적용

--- apps/user/app/(auth)/reset-password/page.tsx
+++ apps/user/app/(auth)/reset-password/page.tsx
@@ -63,7 +63,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 80px;
+  padding-top: 10.25rem;
 `;
 
 const TitleText = styled.h2`
