e80c673532070cb8cde92f83463ba06b921baf24
eunbicho <puppy101976@naver.com>
2025-11-10 10:42:16 +0900

    Fix: 푸터/인증 필요없는 페이지 경로 추가

--- apps/user/lib/constants/routes.ts
+++ apps/user/lib/constants/routes.ts
@@ -1,7 +1,12 @@
 /**
  * 푸터가 필요 없는 페이지 경로 목록
  */
-export const FOOTER_FREE_PAGES = ["/login"];
+export const FOOTER_FREE_PAGES = [
+  "/login",
+  "/reset-password",
+  "/mypage/change-password",
+  "/signup",
+];
 
 /**
  * 인증이 필요 없는 페이지 경로 목록
@@ -11,5 +16,6 @@ export const AUTH_FREE_PAGES = [
   "/reset-password",
   "/change-password",
   "/",
+  "/spaces",
   "/signup",
 ];
